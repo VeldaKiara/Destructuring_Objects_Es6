e341bb9d4b97e9fd7a88d2d74d0c928ed8fa762c
Velda Kiara <veldakarimi@gmail.com>
2020-05-02 18:47:06 +0300

    initial commit

---
+++ objects.js
@@ -0,0 +1,40 @@
+//DESTRUCTURING OBJECTS IN ES6
+//Destructuring is an ES6 expression that makes it
+// possible to unpack values from arrays, or 
+//properties from objects, into distinct variables.
+
+// a user object
+const user = {
+    name : "Maye Edwin",
+    username : "mayeedwin",
+    verified: true,
+    social : {
+      twitter: "mayeedwin1",
+      github: "mayeedwin",
+      site: "https://nerdlyweb.web.app"
+      }
+   }
+//Normally, if you wanted to access and use any key value, below is what you'd do.
+ // print - name, username, site
+ console.log(`
+   Name : ${user.name}
+   Username : ${user.username}
+   Twitter : ${user.social.twitter}
+   Site: ${user.social.site}
+ `) 
+ //how to destructure the object.
+ //Having curly bracket on the left of the equals sign is the new destructuring syntax.
+ 
+ // get name and username first
+ const {name, username} = user;
+        
+ // get the nested social object values on user object
+ const {twitter, site} = user.social;
+
+ // printing user after destructuring
+ console.log(`
+   Name : ${name}
+   Username : ${username}
+   Twitter : ${twitter}
+   Site: ${site}
+ `)
